51b9a07b217adf9b1e9e10845f0e3ff995bad198
Nazar Turkmen <nazar.rudn@gmail.com>
2020-04-21 03:35:53 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,8 @@
-
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
